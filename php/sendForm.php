d3ad61cd8c4b791bd52f3147f4a355f0600dab1d
Junior Melo <juniormelo26@hotmail.com>
2022-06-06 13:08:43 -0300

    Adicionado funcionaliade ao formulario de contato

---
+++ php/sendForm.php
@@ -0,0 +1,27 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+if (!empty($name) && !empty($email) && !empty($subject) && !empty($message)) {
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $receiver = "contato@juniormelo.dev.br";
+        //$receiver = "juniormelo26@hotmail.com";
+        $subject = "Contato pelo site: $subject";
+        $body = "Nome: $name\nEmail: $email\nAssunto: $subject\nMensagem: $message";
+        $sender = "From: $email" . "\n" .
+            "Reply-To: $email" . "\n";
+
+        if (mail($receiver, $subject, $body, $sender)) {
+            echo "Mensagem enviada com sucesso\nEm breve te responderemos, Obrigado!";
+        } else {
+            echo "Ops, falha ao enviar a mensagem!";
+        }
+    } else {
+        echo "Informe um email válido!";
+    }
+} else {
+    echo "Preencha todos os campos!";
+}
